4fff6b325e7d91db1698670dc7cad52863be57f1
Sasha <alecsand.unit@gmail.com>
2024-09-23 13:45:17 +0300

    dz8

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path  = require('path');
-var bodyparser  = require('body-parser');
+var bodyparser = require('body-parser');
 var fs = require('fs');   
 
 var app = express();  
